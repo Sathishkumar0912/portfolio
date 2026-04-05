6cecc74382577a44ba42884087865be02f72d107
Sathishkumar0912 <sathishkumarvvps9360385503@gmail.com>
2026-04-05 22:11:36 +0530

    model changed again

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ app.get("/", (req, res) => {
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 const model = genAI.getGenerativeModel({
-  model: "gemini-1.5-flash-latest",
+    model: "gemini-1.5-pro",
 systemInstruction: `You are the official AI Assistant for Sathishkumar Vengatesan. Your goal is to represent his professional brand with technical precision, accuracy, and professionalism. 
 
 SATHISHKUMAR VENGATESAN
